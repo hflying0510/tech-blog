07a94413d9e6a2ef28b69e5e47c739ce68188361
hflying0510 <eaglebd520@gmail.com>
2024-11-28 17:36:29 +0800

    Update web3-test.py

--- tools/web3-test.py
+++ tools/web3-test.py
@@ -6,7 +6,7 @@ import requests
 
 import csv
 
-with open('./201-info-20241127.csv', ) as f:
+with open('./1.csv', ) as f:
     lines = f.readlines()
     for row in lines:
         Address = row.split(',')[1]
@@ -15,7 +15,7 @@ with open('./201-info-20241127.csv', ) as f:
         Address_bg_name = row.split(',')[4]
         # print("Address：", row.split(',')[1])
         # print("Address_bg_name：", row.split(',')[4])
-        url = f"https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0x2791bca1f2de4661ed88a30c99a7a9449aa84174&address={Address_pm.strip()}&tag=latest&apikey=7JUGEFVWU18VT2TPEMJPTD3J189JDQRTKV"
+        url = f"https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=0x2791bca1f2de4661ed88a30c99a7a9449aa84174&address={Address_pm.strip()}&tag=latest&apikey=7JKV"
         # 获取响应数据 res.text
         # print(url)
         res = requests.get(url=url).text
